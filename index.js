f392f1ca497103098662c222b02d4a78c66e34e8
rufina <rufinka_91@mail.ru>
2022-04-27 19:54:39 +0300

    add second good

--- index.js
+++ index.js
@@ -2,10 +2,23 @@
 
 //  мы всегда используем const  и let
 
-const goodName = 'flat';
-const goodCpunt = 3;
-const goodCategory = 'shoes';
-const goodPrice = 2000;
+{
+    const goodName = 'flat';
+    const goodCount = 3;
+    const goodCategory = 'shoes';
+    const goodPrice = 2000;
 
-console.log('goodName is', goodName);
-console.log('generalGoodsPrice is ', goodCpunt * goodPrice);
+    console.log('goodName is', goodName);
+    console.log(`generalGoodsPrice is ${goodCount * goodPrice}`);
+}
+
+
+{
+    const goodName = 'red hat';
+    const goodCount = 6;
+    const goodCategory = 'hats';
+    const goodPrice = 6000;
+
+    console.log('goodName is', goodName);
+    console.log(`generalGoodsPrice is ${goodCount * goodPrice}`);
+}
