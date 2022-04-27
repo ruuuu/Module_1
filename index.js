1da61e705624fe066f277bacf09bece623691ae6
rufina <rufinka_91@mail.ru>
2022-04-27 19:19:09 +0300

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'; // переключает режим интепретатора на строгий
+
+//  мы всегда используем const  и let
+
+const goodName = 'flat';
+const goodCpunt = 3;
+const goodCategory = 'shoes';
+const goodPrice = 2000;
+
+console.log('goodName is', goodName);
+console.log('generalGoodsPrice is ', goodCpunt * goodPrice);
